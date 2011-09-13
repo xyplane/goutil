f6a015ad818a0aa217ef4d9aabd747b151e6ffb8
Dylan Maxwell <planar.geometry@gmail.com>
2011-09-13 16:25:52 -0600

    Refactor tests to simplify and improve struture for additional tests.

--- config/props_test.go
+++ config/props_test.go
@@ -4,14 +4,12 @@
 package config_test
 
 import(
+	"config"
 	"strings"
 	"testing"
-	"config"
 )
 
-var TestProperties *config.Properties
-
-var TestConfigDataReader = strings.NewReader(`{
+var TestConfigData = `{
 	"bool1":true,
 	"bool2":false,
 	"level2":{
@@ -19,122 +17,90 @@ var TestConfigDataReader = strings.NewReader(`{
 		"bool4":false,
 		"bool5":[ true, false ]
 	}
-}`)
+}`
 
-func TestReadProperties(t *testing.T) {
-	props, err := config.ReadProperties(TestConfigDataReader)
+func TestBool(t *testing.T) {
+
+	t.Log("Read the following JSON config data:\n" + TestConfigData)
+
+	properties, err := config.ReadProperties(strings.NewReader(TestConfigData))
 	if err == nil {
-		t.Log("Success reading config properties from JSON string.")
+		t.Log("Success reading config properties.")
 	} else {
-		t.Fatal("Error reading config properties from JSON string:", err)
-	}
-	TestProperties = props
-}
-
-func TestBool1(t *testing.T) {
-	if(TestProperties == nil) {
-		t.FailNow()
-	}
-
-	b, err := TestProperties.Bool("bool1")
-	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool1'.")
-	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool1':", err)
-	}
-
-	if b == true {
-		t.Log("Bool value for 'bool1' is true.")
-	} else {
-		t.Fatal("Bool value for 'bool1' is not true.")
-	}
-}
-
-func TestBool2(t *testing.T) {
-	if(TestProperties == nil) {
-		t.FailNow()
-	}
-
-	b, err := TestProperties.Bool("bool2")
-	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool2'.")
-	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool2':", err)
-	}
-
-	if b == false {
-		t.Log("Bool value for 'bool2' is false.")
-	} else {
-		t.Fatal("Bool value for 'bool2' is not false.")
-	}
-}
-
-func TestBool3(t *testing.T) {
-	if(TestProperties == nil) {
-		t.FailNow()
-	}
-
-	b, err := TestProperties.Bool("level2.bool3")
-	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool3'.")
-	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool3':", err)
-	}
-
-	if b == true {
-		t.Log("Bool value for 'bool3' is true.")
-	} else {
-		t.Fatal("Bool value for 'bool3' is not true.")
-	}
-}
-
-func TestBool4(t *testing.T) {
-	if(TestProperties == nil) {
-		t.FailNow()
-	}
-
-	b, err := TestProperties.Bool("level2", "bool4")
-	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool4'.")
-	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool4':", err)
-	}
-
-	if b == false {
-		t.Log("Bool value for 'bool4' is false.")
-	} else {
-		t.Fatal("Bool value for 'bool4' is not false.")
-	}
-}
-
-func TestBool5(t *testing.T) {
-	if(TestProperties == nil) {
-		t.FailNow()
-	}
-
-	b, err := TestProperties.Bool("level2.bool5[0]")
-	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool5[0]'.")
-	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool5[0]':", err)
-	}
-
-	if b == true {
-		t.Log("Bool value for 'bool5[0]' is true.")
-	} else {
-		t.Fatal("Bool value for 'bool5[0]' is not true.")
+		t.Fatal("Error reading config properties:", err)
 	}
 	
-	b, err = TestProperties.Bool("level2", "bool5", 1)
+	var b bool
+
+	b, err = properties.Bool("bool1")
 	if err == nil {
-		t.Log("Success retrieving Bool value from property 'bool5[1]'.")
+		if b == true {
+			t.Log("Bool value for 'bool1' is true.")
+		} else {
+			t.Error("Bool value for 'bool1' is not true.")
+		}
 	} else {
-		t.Fatal("Error retrieving Bool value from property 'bool5[1]':", err)
+		t.Error("Error getting Bool value from property 'bool1':", err)
 	}
 
-	if b == false {
-		t.Log("Bool value for 'bool5[1]' is false.")
+
+	b, err = properties.Bool("bool2")
+	if err == nil {
+		if b == false {
+			t.Log("Bool value for 'bool2' is false.")
+		} else {
+			t.Error("Bool value for 'bool2' is not false.")
+		}
 	} else {
-		t.Fatal("Bool value for 'bool5[1]' is not false.")
+		t.Error("Error getting Bool value from property 'bool2':", err)
+	}
+
+
+	b, err = properties.Bool("level2.bool3")
+	if err == nil {
+		if b == true {
+			t.Log("Bool value for 'bool3' is true.")
+		} else {
+			t.Error("Bool value for 'bool3' is not true.")
+		}
+	} else {
+		t.Error("Error getting Bool value from property 'bool3':", err)
+	}
+
+
+	b, err = properties.Bool("level2", "bool4")
+	if err == nil {	
+		if b == false {
+			t.Log("Bool value for 'bool4' is false.")
+		} else {
+			t.Error("Bool value for 'bool4' is not false.")
+		}
+	} else {
+		t.Error("Error getting Bool value from property 'bool4':", err)
+	}
+
+
+	b, err = properties.Bool("level2.bool5[0]")
+	if err == nil {
+		if b == true {
+			t.Log("Bool value for 'bool5[0]' is true.")
+		} else {
+			t.Error("Bool value for 'bool5[0]' is not true.")
+		}
+	} else {
+		t.Error("Error getting Bool value from property 'bool5[0]':", err)
+	}
+	
+
+	b, err = properties.Bool("level2", "bool5", 1)
+	if err == nil {
+		if b == false {
+			t.Log("Bool value for 'bool5[1]' is false.")
+		} else {
+			t.Error("Bool value for 'bool5[1]' is not false.")
+		}
+	} else {
+		t.Error("Error retrieving Bool value from property 'bool5[1]':", err)
 	}
 }
+
