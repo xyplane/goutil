0f32e156f7e78a7b763c768483e671e04186f790
Dylan Maxwell <planar.geometry@gmail.com>
2011-09-13 21:10:37 -0600

    Add support for reading config properties from config file.

--- config/file.go
+++ config/file.go
@@ -1,12 +1,32 @@
+//
+//
+//
 package config
 
 import (
-
+	"os"
 )
 
 type ConfigFile struct {
-	Properties
+	*Properties
 	fname string
+}
+
+// ReadConfigFile reads the specified file and reads the config properties.
+func ReadConfigFile(fname string) (c *ConfigFile, err os.Error) {
+	var f *os.File
+	f, err = os.Open(fname)
+	if err != nil {
+		return
+	}
+	
+	var p *Properties
+	p, err = ReadProperties(f)
+	if err != nil {
+		return
+	}
+	
+	return &ConfigFile{ p, fname }, nil
 }
 
 func (c *ConfigFile) SetFileName(fname string) {
