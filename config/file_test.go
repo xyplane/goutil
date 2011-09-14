363bd9b87df3a4e9cb81f04dee29d971e931995f
Dylan Maxwell <planar.geometry@gmail.com>
2011-09-13 21:28:07 -0600

    Add tests for config file support.

---
+++ config/file_test.go
@@ -0,0 +1,35 @@
+//
+//
+//
+package config_test
+
+import(
+	"config"
+	"testing"
+)
+
+var TestFileName = "testdata/config.json"
+
+func TestFile(t *testing.T) {
+
+	t.Log("Read config properties from file:", TestFileName)
+
+	c, err := config.ReadConfigFile(TestFileName)
+	if err == nil {
+		t.Log("Success reading test config file")
+	} else {
+		t.Fatal("Error reading test config file:", err)
+	}
+	
+	var host string
+	host, err = c.String("host")
+	if err == nil {
+		if host == "localhost" {
+			t.Log("String value for property 'host' is 'localhost'.")
+		} else {
+			t.Error("String value for property 'host' is not 'localhost'.")
+		}
+	} else {
+		t.Error("Error getting string value from property 'host':", err)
+	}
+}
